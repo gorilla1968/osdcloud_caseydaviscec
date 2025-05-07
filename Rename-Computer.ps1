6b11daf3301ba1eb7d17c025699d5f36d4e7722b
Casey Davis <130083515+caseydaviscec@users.noreply.github.com>
2025-05-07 15:25:37 -0600

    Create Rename-Computer.ps1

---
+++ Rename-Computer.ps1
@@ -0,0 +1,8 @@
+#Start the Transcript
+$Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OSDCloud.log"
+$null = Start-Transcript -Path (Join-Path "$env:SystemRoot\Temp" $Transcript) -ErrorAction Ignore
+
+# Setting the hostname
+Write-Host -ForegroundColor Red "Rename Computer before Autopilot"
+$Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
+Rename-Computer -Newname CEC-$AssignedComputerName -Force -Reboot
