e38b6ad9a3a4a68cdf2dc736757ca687ec778c8d
Casey Davis <130083515+caseydaviscec@users.noreply.github.com>
2025-05-09 08:17:06 -0600

    Create Rename-LabComputer.ps1

---
+++ Rename-LabComputer.ps1
@@ -0,0 +1,13 @@
+# Transcript for logging
+$stampDate = Get-Date
+$scriptName = ([System.IO.Path]::GetFileNameWithoutExtension($(Split-Path $script:MyInvocation.MyCommand.Path -Leaf)))
+$logFile = "$env:OSDCloud\Logs\$scriptName-" + $stampDate.ToFileTimeUtc() + ".log"
+Start-Transcript -Path $logFile -NoClobber
+$VerbosePreference = "Continue"
+
+# Set Hostname before Autopilot
+Write-Host -ForegroundColor Red "Rename Computer before Autopilot"
+$Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
+Rename-Computer -Newname CECLab-$Serial -Restart
+
+Stop-Transcript
