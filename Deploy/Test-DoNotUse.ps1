dd0cd15f5664348f0341410da0e491c9ce25645c
Casey Davis <casey.davis@coloradoearlycolleges.org>
2025-05-19 10:29:39 -0600

    Enabled debug prompt for Test-DoNotUse profile

--- Deploy/Test-DoNotUse.ps1
+++ Deploy/Test-DoNotUse.ps1
@@ -136,7 +136,7 @@ start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scri
 # start /wait powershell.exe -NoL -ExecutionPolicy Bypass -F C:\Windows\Setup\Scripts\autopilot.ps1
 
 # Below a PS session for debug and testing in system context, # when not needed 
-# start /wait powershell.exe -NoL -ExecutionPolicy Bypass
+start /wait powershell.exe -NoL -ExecutionPolicy Bypass
 
 exit 
 '@
