1b670cb050d5f371b981a084e796bbacad7db4ed
TJ King <tj.king@coloradoearlycolleges.org>
2025-06-03 09:36:48 -0600

    Rolling Autopilot.ps1 back to commit dcab67b

--- Autopilot.ps1
+++ Autopilot.ps1
@@ -365,7 +365,7 @@ Function CustomWindowsAutopilotInfo {
 				$deviceCount = $imported.Length
 				Write-Host "Waiting for $processingCount of $deviceCount to be imported"
 				if ($processingCount -gt 0) {
-					Start-Sleep 90
+					Start-Sleep 30
 				}
 			}
 			$importDuration = (Get-Date) - $importStart
@@ -397,7 +397,7 @@ Function CustomWindowsAutopilotInfo {
 				$deviceCount = $autopilotDevices.Length
 				Write-Host "Waiting for $processingCount of $deviceCount to be synced"
 				if ($processingCount -gt 0) {
-					Start-Sleep 90
+					Start-Sleep 30
 				}
 			}
 			$syncDuration = (Get-Date) - $syncStart
@@ -451,12 +451,15 @@ Function CustomWindowsAutopilotInfo {
 					$deviceCount = $autopilotDevices.Length
 					Write-Host "Waiting for $processingCount of $deviceCount to be assigned"
 					if ($processingCount -gt 0) {
-						Start-Sleep 90
+						Start-Sleep 30
 					}	
 				}
 				$assignDuration = (Get-Date) - $assignStart
 				$assignSeconds = [Math]::Ceiling($assignDuration.TotalSeconds)
 				Write-Host "Profiles assigned to all devices.  Elapsed time to complete assignment: $assignSeconds seconds"	
+				if ($Reboot) {
+					Restart-Computer -Force
+				}
 			}
 		}
 	}
@@ -500,11 +503,9 @@ CustomWindowsAutopilotInfo @Params
 Write-SectionHeader "Disconnect Graph API"
 Disconnect-MgGraph | Out-Null
 
-
+Write-DarkGrayHost "Remove Senstive Data"
 if (Test-Path -Path $env:SystemDrive\OSDCloud\Scripts\osdcloud.shh) {
-	Write-DarkGrayHost "Removing Sensitive Data"
 	Remove-Item -Path $env:SystemDrive\OSDCloud\Scripts\osdcloud.shh -Force
 }
 
 Stop-Transcript | Out-Null
-Restart-Computer -Force
