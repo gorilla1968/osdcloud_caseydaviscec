d2bb0e9427af7ea48c7a960acf12a40ca2f4d091
TJ King <tj.king@coloradoearlycolleges.org>
2025-06-11 15:53:43 -0600

    Added -force to rename to catch edge cases

--- Rename-Computer.ps1
+++ Rename-Computer.ps1
@@ -8,6 +8,6 @@ $VerbosePreference = "Continue"
 # Set Hostname before Autopilot
 Write-Host -ForegroundColor Red "Rename Computer before Autopilot"
 $Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
-Rename-Computer -Newname CEC-$Serial -Restart
+Rename-Computer -Newname CEC-$Serial -Restart -Force
 
 Stop-Transcript
